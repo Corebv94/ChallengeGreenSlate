cdfef28595fc794811e9307553f4263c01234e88
Bryan <corea657@gmail.com>
2019-09-30 20:38:10 -0600

    project

---
+++ WebApplication2/Controllers/GeneralController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    public class GeneralController : Controller
+    {
+        // GET: General
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList() {
+            using (var db = new ChallengeEntities()) {
+                db.Configuration.LazyLoadingEnabled = false;
+                var context = (from up in db.UserProjects
+                               join u in db.User on up.UserId equals u.Id
+                               join p in db.Project on up.ProjectId equals p.Id
+                               select new
+                               {
+                                   ProjectId = up.ProjectId,
+                                   StartDate = p.StartDate.ToString(),
+                                   TimeToStart = DbFunctions.DiffDays(p.EndDate, p.StartDate) > 0 ? DbFunctions.DiffDays(p.EndDate, p.StartDate).ToString() : "Started" ,
+                                   EndDate = p.EndDate.ToString(),
+                                   Credits = p.Credits,
+                                   Status = up.IsActive
+
+                               }).ToList();
+                return Json(new { data = context}, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ WebApplication2/Controllers/UserProjectsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserProjectsController : Controller
+    {
+        private ChallengeEntities db = new ChallengeEntities();
+
+        // GET: UserProjects
+        public ActionResult Index()
+        {
+            var userProjects = db.UserProjects.Include(u => u.Project).Include(u => u.User);
+            return View(userProjects.ToList());
+        }
+
+        // GET: UserProjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProjects userProjects = db.UserProjects.Find(id);
+            if (userProjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProjects);
+        }
+
+        // GET: UserProjects/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectId = new SelectList(db.Project, "Id", "Id");
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: UserProjects/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,ProjectId,IsActive")] UserProjects userProjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProjects.Add(userProjects);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectId = new SelectList(db.Project, "Id", "Id", userProjects.ProjectId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", userProjects.UserId);
+            return View(userProjects);
+        }
+
+        // GET: UserProjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProjects userProjects = db.UserProjects.Find(id);
+            if (userProjects == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectId = new SelectList(db.Project, "Id", "Id", userProjects.ProjectId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", userProjects.UserId);
+            return View(userProjects);
+        }
+
+        // POST: UserProjects/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,ProjectId,IsActive")] UserProjects userProjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userProjects).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectId = new SelectList(db.Project, "Id", "Id", userProjects.ProjectId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", userProjects.UserId);
+            return View(userProjects);
+        }
+
+        // GET: UserProjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProjects userProjects = db.UserProjects.Find(id);
+            if (userProjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProjects);
+        }
+
+        // POST: UserProjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProjects userProjects = db.UserProjects.Find(id);
+            db.UserProjects.Remove(userProjects);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
